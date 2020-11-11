25680042bba68bfd8698b1bc824c2aa1e5732039
Tucker Leach <leachtucker@gmail.com>
2020-11-10 17:57:46 -0800

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      {/* Signup */}
-      {/* Login */}
-      {/* Dashboard */}
+      <Router>
+        {/* Signup */}
+        {/* Login */}
+        {/* Dashboard */}
+      </Router>
     </div>
   );
 }
