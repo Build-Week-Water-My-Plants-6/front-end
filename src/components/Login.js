2885cdc84e1dcf17be6da19e41986dc31567d2ad
Tucker <leachtucker@gmail.com>
2020-11-11 16:38:37 -0800

    Setup some routes and components

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,31 +6,33 @@ function Login() {
     // Setup component state for the form management
 
     // Write event handlers (onChange, onSubmit)
-    
+
     return (
         <div className="login box py-5 px-6">
             <div className="box-header has-text-centered mb-6">
                 <h2 className="title is-4">Login</h2>
             </div>
             <form className="mx-6">
-                <div class="field">
-                    <label class="label">Username</label>
-                    <div class="control">
-                        <input class="input" type="text" />
+                <div className="field">
+                    <label className="label">Username</label>
+                    <div className="control">
+                        <input className="input" type="text" />
                     </div>
                 </div>
-                <div class="field">
-                    <label class="label">Password</label>
-                    <div class="control">
-                        <input class="input" type="text" />
+                <div className="field">
+                    <label className="label">Password</label>
+                    <div className="control">
+                        <input className="input" type="text" />
                     </div>
                 </div>
-                <div className="control has-text-centered mt-5 mb-1">
-                    <button className="button">Login</button>
+                <div className="field is-flex is-justify-content-center mt-5 mb-1">
+                    <div className="control">
+                        <button className="button">Login</button>
+                    </div>
                 </div>
             </form>
         </div>
-    )
+    );
 };
 
 export default Login;
